556b76ea0ee2f570e9cd846a4cfb461157d44760
andrewni <ajni@ucdavis.edu>
2017-09-20 13:53:04 -0700

    updated functional_tests.py, home.html, tests.py, and views.py

--- lists/tests.py
+++ lists/tests.py
@@ -11,14 +11,19 @@ class HomePageTest(TestCase):
     found = resolve('/')
     self.assertEqual(found.func, home_page)
 
-  def test_home_page_returns_correct_html(self):
+  def test_uses_home_template(self):
     # request = HttpRequest()
     # response = home_page(request)
-    response = self.client.get('/')
     # html = response.content.decode('utf8')
     # self.assertTrue(html.startswith('<!DOCTYPE html>'))
     # self.assertIn('<title>To-Do lists</title>', html)
     # self.assertTrue(html.strip().endswith('</html>'))
     # expected_html = render_to_string('home.html')
     # self.assertEqual(html, expected_html)
-    self.assertTemplateUsed(response, 'home.html')
+    response = self.client.get('/')
+    self.assertTemplateUsed(response, 'lists/home.html')
+
+  def test_can_save_a_POST_request(self):
+    response = self.client.post('/', data={'item_text':'A new list item'})
+    self.assertIn('A new list item', response.content.decode())
+    self.assertTemplateUsed(response, 'lists/home.html')
