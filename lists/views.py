9b00b54a3c8eacc56f1bc85997279f3a60d3f9d9
andrewni <ajni@ucdavis.edu>
2017-09-21 18:07:55 -0700

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,9 @@ from lists.models import Item
 def home_page(request):
   if request.method == 'POST':
     Item.objects.create(text=request.POST['item_text'])
-    return redirect('/')
-
-  items = Item.objects.all()
-  return render(request, 'lists/home.html', {'items': items})
+    return redirect('/lists/the-only-list-in-the-world/')
+  # items = Item.objects.all()
+  return render(request, 'lists/home.html')
   # if request.method == 'POST':
   #   new_item_text = request.POST['item_text']
   #   Item.objects.create(text=new_item_text)
@@ -19,3 +18,6 @@ def home_page(request):
   #     'new_item_text': new_item_text,
   #   })
   
+def view_list(request):
+  items = Item.objects.all()
+  return render(request, 'lists/list.html', {'items': items})
